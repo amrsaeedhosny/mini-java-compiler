625c48d0f3d790f3dba804e1ba057d98aee18315
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2018-03-14 16:09:38 +0200

    Abood work added

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 import items.TokenRegex;
 
 public class LexicalAnalyzer {
@@ -26,6 +27,15 @@ public class LexicalAnalyzer {
 				int endIndex = matcher.end();
 				
 				Token token = new Token(tokenType, foundToken, startIndex, endIndex);
+				boolean check =false;
+				for(int i = 0 ; i < matchedTokens.size() ; i++){
+					if(startIndex>=matchedTokens.get(i).getStartIndex()&&endIndex<=matchedTokens.get(i).getEndIndex())
+					{
+						check=true;
+						break;
+					}
+				}
+				if(!check)
 				matchedTokens.add(token);
 								
 			}
@@ -36,24 +46,39 @@ public class LexicalAnalyzer {
 		// sort the found token by their startIndex
 		sortMatchedTokens(matchedTokens);
 		
+		//remove duplicate
+		for(int i = 0 ; i < matchedTokens.size()-1 ; i++)
+		{
+			if(matchedTokens.get(i).getStartIndex()==matchedTokens.get(i+1).getStartIndex())
+			{
+				matchedTokens.remove(i+1);
+				i--;
+			}
+		}
+		
+		
+		
 		
 		/* Check for unknown (Error) tokens */
 		// check for unknown token in the middle of the code
-		for (int i = 0; i<matchedTokens.size()-2; i++) { 
+		ArrayList<Token> temp = new ArrayList<>();
+		for (int i = 0; i<matchedTokens.size()-1; i++) {
 			Token currentToken = matchedTokens.get(i);
 			Token nextToken = matchedTokens.get(i+1);
 	
 			int currentTokenEndIndex = currentToken.getEndIndex();
 			int nextTokenStartIndex = nextToken.getStartIndex();
-			
-			if (currentTokenEndIndex != nextTokenStartIndex) {
+			if (currentTokenEndIndex != nextTokenStartIndex && currentTokenEndIndex!=nextTokenStartIndex-1) {
 				// caught an error
 				Token unknownToken = new Token(Token.UNKNOWN_TOKEN_TYPE, 
 						code.substring(currentTokenEndIndex, nextTokenStartIndex),
 						currentTokenEndIndex, nextTokenStartIndex);
-				matchedTokens.add(unknownToken);
+				temp.add(unknownToken);
 			}
 
+		}
+		for(int i = 0 ; i < temp.size() ; i++){
+			matchedTokens.add(temp.get(i));
 		}
 		
 		// check for unknown token at the very beginning & at the very end
@@ -67,11 +92,11 @@ public class LexicalAnalyzer {
 					code.substring(0, matchedTokens.get(0).getStartIndex()),
 					0, matchedTokens.get(0).getStartIndex());
 		}
-		if ( (matchedTokens.get(matchedTokens.size()-1).getEndIndex()) != (code.length()) ) {
+		if ( (matchedTokens.get(matchedTokens.size()-1).getEndIndex())-1 != (code.length()-1) ) {
 			// caught an error
 			unknownToken2 = new Token(Token.UNKNOWN_TOKEN_TYPE, 
-					code.substring(matchedTokens.get(matchedTokens.size()-1).getEndIndex(), code.length())
-							,matchedTokens.get(matchedTokens.size()-1).getEndIndex() ,code.length());
+					code.substring(matchedTokens.get(matchedTokens.size()-1).getEndIndex()-1, code.length()-1)
+							,matchedTokens.get(matchedTokens.size()-1).getEndIndex()-1 ,code.length());
 		}
 		if (unknownToken1 != null) matchedTokens.add(unknownToken1);
 		if (unknownToken2 != null) matchedTokens.add(unknownToken2);
@@ -79,7 +104,11 @@ public class LexicalAnalyzer {
 		// Again, sort the found token by their startIndex
 		sortMatchedTokens(matchedTokens);
 		
-		System.out.println(matchedTokens);
+		//System.out.println(matchedTokens);
+		for(int i = 0 ; i < matchedTokens.size() ; i++)
+		{
+			System.out.println("<"+matchedTokens.get(i).getType()+"> : "+" "+matchedTokens.get(i).getValue());
+		}
 		return matchedTokens;
 	}
 	
@@ -97,5 +126,5 @@ public class LexicalAnalyzer {
 		    }
 		});
 	}
-
+	
 }
